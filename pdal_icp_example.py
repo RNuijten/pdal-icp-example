25a8736635a12422e439517dd7553b308a55ebfb
RNuijten <riknuijten@live.nl>
2024-03-29 18:13:16 +0100

    Update pdal_icp_example.py
    In development.
    
    - Fixed issues with depreciated PDAL commands
    - Can write text file with transformation matrices
    - Saves figure
    
    To add:
    - Derive bounds from original point cloud
    - Set minimum number of points needed for ICP, as point clouds are often irregular shaped instead of square
    - Add methods to get average/median rotation, translation, scaling...

--- pdal_icp_example.py
+++ pdal_icp_example.py
@@ -1,25 +1,39 @@
 import json
 import matplotlib.pyplot as plt
 import pdal
-
+import os
+from tqdm import tqdm
 
 # ICP window and step sizes
 window_size = 200
 step_size = 100
 
-# Path to pre- and post-event indexed point clouds (EPT)
-pre_event_index = './data/ept/new/bv_06/ept.json'
-post_event_index = './data/ept/new/bv_09/ept.json'
+# Minimum points required for ICP?
+min_points = 400000
 
-# Variables to hold the ICP vector origins (X, Y) and displacements (dx, dy)
+# Path to pre- and post-event indexed point clouds (.las or .laz)
+pre_event_index = './data/source.laz'  # is this the source?
+post_event_index = './target.laz' # is this the target?
+
+# Store transformation matrices in .txt output?
+text_out = True
+
+# Variables to hold full matrices (T), as well as the ICP vector origins (X, Y) and displacements (dx, dy)
+T = []
 X = []
 Y = []
 dx = []
 dy = []
 
+
+# Retrieve bounds fom point cloud (xmin, xmax, ymin, ymax)
+
+
+# Can we add an option to create a median/ mean tranformation matrix?
+
 # Slide a window through the analysis area
-for x in range(556627, 558726, step_size):
-    for y in range(4238930, 4240977, step_size):
+for x in tqdm(range(556627, 558726, step_size), leave=True):
+    for y in tqdm(range(4238930, 4240977, step_size), leave=False):
 
         # PDAL pipeline with data bounds set according to the current window.
         # The first window is 'fixed'; The second window is 'moving'.
@@ -27,7 +41,7 @@ for x in range(556627, 558726, step_size):
         # move within the fixed window as the ICP solution converges.
         pipeline = [
             {
-                'type':'readers.ept',
+                'type':'readers.las',
                 'filename':post_event_index,
                 'bounds':'([{},{}],[{},{}])'.format(x - 2,
                                                     x + window_size + 2,
@@ -35,7 +49,7 @@ for x in range(556627, 558726, step_size):
                                                     y + window_size + 2)
             },
             {
-                'type':'readers.ept',
+                'type':'readers.las',
                 'filename':pre_event_index,
                 'bounds':'([{},{}],[{},{}])'.format(x,
                                                     x + window_size,
@@ -49,7 +63,6 @@ for x in range(556627, 558726, step_size):
 
         # Execute the pipeline
         p = pdal.Pipeline(json.dumps(pipeline))
-        p.validate()
         p.execute()
 
         # Capture the metadata, which contains the ICP transformation
@@ -59,6 +72,7 @@ for x in range(556627, 558726, step_size):
         # Store vector origin and ICP-derived displacement
         try:
             t = [float(val) for val in t.split()]
+            T.append(t)
             X.append(x + window_size/2)
             Y.append(y + window_size/2)
             dx.append(t[3])
@@ -74,4 +88,16 @@ for x in range(556627, 558726, step_size):
 plt.figure()
 plt.quiver(X, Y, dx, dy, angles='xy', scale_units='xy')
 plt.axis('equal')
-plt.show()
+
+# Save
+img_directory = './img/'
+if not os.path.exists(img_directory):
+    os.makedirs(img_directory) # Create the directory if it doesn't exist
+plt.savefig(img_directory+'icp_vectors.png', dpi=100)
+
+# Save .txt file with transformation matrices
+if text_out == True:
+    with open('./data/matrices.txt', mode='wt', encoding='utf-8') as myfile:
+        for i in range(len(T)):
+            myfile.write('x='+str(X[i]), ', y='+str(Y[i])+'\n')
+            myfile.write(str(T[i])+'\n')
